fc9005dbb5d9366b099d46bec3de4ef369fba9d8
Maxime Tournier <mxm.tournier@gmail.com>
2023-04-23 18:23:53 +0200

    update

--- script.js
+++ script.js
@@ -7,25 +7,14 @@ TOUT COPIER DANS LA PARTI "JS"
 document.addEventListener('goalLoad', function(obj) {
 
   console.log(obj.detail);
-  
+
   let OBJECTIF = obj.detail.amount.target
   let VALUE = obj.detail.amount.current
 
+  const TEXT_VALUE = document.querySelector('.subtext');
+  TEXT_VALUE.innerHTML = VALUE
+
   setSpinnerValue(VALUE, OBJECTIF);
-
-  function setSpinnerValue(value, objectif) {
-
-    if(value > objectif){ value = objectif }  
-    const circle = document.querySelector('.spinner-circle');
-    const radius = circle.r.baseVal.value;
-    const circumference = 2 * Math.PI * radius;
-    const progress = value / objectif;
-    const dashoffset = circumference * (1 - progress);
-    circle.style.strokeDashoffset = dashoffset;
-    circle.parentNode.setAttribute('data-value', value);
-
-  }
-  
   
 });
 
@@ -34,3 +23,16 @@ document.addEventListener('goalEvent', function(obj) {
   console.log(obj.detail);
 
 });
+
+function setSpinnerValue(value, objectif) {
+
+  if(value > objectif){ value = objectif }  
+  const circle = document.querySelector('.spinner-circle');
+  const radius = circle.r.baseVal.value;
+  const circumference = 2 * Math.PI * radius;
+  const progress = value / objectif;
+  const dashoffset = circumference * (1 - progress);
+  circle.style.strokeDashoffset = dashoffset;
+  circle.parentNode.setAttribute('data-value', value);
+
+}
